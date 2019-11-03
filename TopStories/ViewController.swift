89c56cf1437eaabfbf12d2aea4b743ae4fee228e
Abdullah Bayraktar <abdullahbayraktar9@gmail.com>
2019-11-03 12:49:46 +0300

    Initial Commit

---
+++ TopStories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TopStories
+//
+//  Created by Abdullah Bayraktar on 3.11.2019.
+//  Copyright © 2019 Abdullah Bayraktar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
